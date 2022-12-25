14db9b2a6f4f61e7ba56d9ed4e9c37cf60aa3735
Stavros Nikolaidis <stavniko@csd.auth.gr>
2022-12-25 12:58:26 +0200

    4 features

--- main.js
+++ main.js
@@ -29,13 +29,19 @@ client.on('messageCreate', msg => {
     }
     //console.log(cs)
 
-    if (result[0] === 'getUser')
+    if (result[0] === '>user')
         getUser(cs, msg, rpm);
-    if (result[0] === 'getPictures')
+    if (result[0] === '>pics')
         getPictures(cs, msg, rpm);
-    if(result[0] === 'getStats')
+    if(result[0] === '>stats')
         getStats(cs, msg, rpm);
+    if(result[0] === '>info')
+        getInfo(cs, msg, rpm);
 
+    if(result[0] === '>supp')
+        printSupport(msg);
+    if(result[0] === '>help')
+        printHelp(msg);
 })
 
 function getUser(cs, msg, rpm){
@@ -69,20 +75,77 @@ function getStats(cs, msg, rpm){
         const chart = new QuickChart();
         chart.setConfig({
             type: 'bar',
-            data: { labels: ['Watching', 'Completed', 'On Hold', 'Dropped', 'PTW'],
-                datasets: [{ label: 'User State', data: [states[0], states[1], states[2], states[3], states[4] ] }] },
+            data: {
+                labels: ['Watching', 'Completed', 'On Hold', 'Dropped', 'PTW'],
+                datasets: [{
+                    label: 'User State',
+                    data: [states[0], states[1], states[2], states[3], states[4]],
+                    backgroundColor: '#4effdf'
+                }]
+            },
+            options: {
+                plugins: {
+                    datalabels: {
+                        color: '#fff'
+                    }
+                }
+            }
         });
         let url = await chart.getShortUrl();
         msg.channel.send(`User States: ${url}`);
         chart.setConfig({
             type: 'bar',
             data: { labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-                datasets: [{ label: 'User Rating', data: [states[15], states[14], states[13], states[12], states[11],
-                        states[10], states[9], states[8], states[7], states[6] ] }] },
+                    datasets: [{ label: 'User Rating', data: [states[15], states[14], states[13], states[12], states[11],
+                                                                states[10], states[9], states[8], states[7], states[6] ] }]
+            },
+            options: {
+                plugins: {
+                    datalabels: {
+                        color: '#fff'
+                    }
+                }
+            }
         });
         url = await chart.getShortUrl();
         msg.channel.send(`User Rating: ${url}`);
     }).catch((err) => console.log(err))
+}
+
+function getInfo(cs, msg, rpm){
+    malScraper.getInfoFromName(cs).then((data) => {
+        //rpm = rpm + '```\n'
+        for(var prop in data) {
+            if(prop !== 'characters' && prop !== 'staff')
+                rpm = rpm + '**' + prop + ':**' + '\t' + data[prop] + '\n'
+        }
+        //rpm = rpm + '```'
+        msg.channel.send(rpm)
+    })
+}
+
+function printHelp(msg) {
+    const txt = '>>> Commands\n' +
+                '**>user** username: sends info for the user\n' +
+                '**>stats** anime_name: sends stats in diagrams for an anime\n' +
+                '**>pics** anime_name: sends pictures of an anime\n' +
+                '**>info** anime_name: sends information for an anime\n' +
+                '**>supp**: a command that show you all the ways you can support the MALBot project'
+
+    msg.channel.send(txt);
+
+}
+
+function printSupport(msg){
+    const txt = '`You can support the MALBot Project and me (the developer) by the following ways:` \n' +
+                ':star: Star the project on GitHub: https://github.com/StavrosNik4/MALBot \n' +
+                ':arrow_forward: Watch my YouTube video for the bot (it\'s in Greek but there are English subtitles): \n' +
+                ':moneybag: PayPal: https://www.paypal.com/donate/?hosted_button_id=63 \n' +
+                ':coffee: or buy me a Ko-fi: https://ko-fi.com/kamenos\n' +
+                ':star: Add the bot to your own Discord server: https://discord.com/api/oauth2/authorize?client_id=1039731156125503560&permissions=8&scope=bot%20applications.commands\n' +
+                'Thank you for all your support! :heart:'
+
+    msg.channel.send(txt);
 }
 
 client.login(process.env.TOKEN)
