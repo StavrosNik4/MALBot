18ad20e45ecabf9a733cc4ebd3445e1cde60a44d
Stavros Nikolaidis <stavniko@csd.auth.gr>
2023-09-16 23:38:08 +0300

    Server Logging v1

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const {getUser} = require('./user.js');
 const {getStats} = require('./stats.js');
 const {getPictures} = require('./pictures.js');
 const {getInfo} = require('./info.js');
+const fs = require("fs");
 
 
 // create the client
@@ -20,6 +21,50 @@ const client = new Client({
 client.on('ready', () => {
     console.log('Ready!')
 })
+
+// Event listener for when the bot joins a server (guild)
+client.on('guildCreate', (guild) => {
+    // Log the server join event to the console
+    console.log(`Bot joined server: ${guild.name} (ID: ${guild.id})`);
+    // Call the function to log the server join event to a file
+    logServerEvent('joined', guild);
+});
+
+// Event listener for when the bot leaves a server (guild)
+client.on('guildDelete', (guild) => {
+    // Log the server leave event to the console
+    console.log(`Bot left server: ${guild.name} (ID: ${guild.id})`);
+    // Call the function to log the server leave event to a file
+    logServerEvent('left', guild);
+});
+
+// Function to get the current timestamp in a human-readable format
+function getCurrentTimestamp() {
+    const now = new Date();
+    // Format the date as 'YYYY-MM-DD'
+    const formattedDate = now.toLocaleDateString('en-US');
+    // Format the time as 'HH:MM:SS' in 24-hour format
+    const formattedTime = now.toLocaleTimeString('en-US', { hour12: false });
+    // Combine the date and time for the full timestamp
+    return `${formattedDate} at ${formattedTime}`;
+}
+
+// Function to log a server event to a file
+function logServerEvent(eventType, guild) {
+    // Define the log file path based on the event type (joined or left)
+    const logFilePath = `./${eventType}_servers.txt`;
+    // Get the current timestamp
+    const timestamp = getCurrentTimestamp();
+
+    // Append the event information to the log file
+    fs.appendFile(logFilePath, `${timestamp} - ${guild.name} (ID: ${guild.id})\n`, (err) => {
+        if (err) {
+            // Handle any errors that occur during file writing
+            console.error('Error writing to log file:', err);
+        }
+    });
+}
+
 
 // how the bot reacts to messages
 client.on('messageCreate', msg => {
