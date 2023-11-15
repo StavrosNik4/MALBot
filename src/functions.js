2e7f7c118490699b00aace153d256b3ebf758b88
Stavros Nikolaidis <stavniko@csd.auth.gr>
2023-11-15 22:29:10 +0200

    moved the logCommandEvent function from main.js to functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 /**
  * Remove all non-alphanumeric characters from the string
  * @param str the given string
@@ -7,6 +9,50 @@ function removeNonAlphanumeric(str) {
     return str.replace(/[^a-zA-Z0-9]/g, '');
 }
 
+function logCommandEvent(command){
+    const filePath = `./commands.txt`;
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading the file: ${err}`);
+            return;
+        }
+
+        // Create a map to store the values for each command
+        const valueMap = new Map();
+
+        // Split the file content by lines and parse each line
+        data.split('\n').forEach((line) => {
+            const [key, value] = line.trim().split(':');
+            if (key && value) {
+                valueMap.set(key.trim(), parseInt(value.trim()));
+            }
+        });
+
+        // Increment the value for the specified command
+        if (valueMap.has(command)) {
+            valueMap.set(command, valueMap.get(command) + 1);
+
+            // Generate the updated content
+            const updatedContent = Array.from(valueMap)
+                .map(([key, value]) => `${key}: ${value}`)
+                .join('\n');
+
+            // Write the updated content back to the file
+            fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
+                if (err) {
+                    console.error(`Error writing to the file: ${err}`);
+                    return;
+                }
+                console.log(`${command} incremented successfully!`);
+            });
+        } else {
+            console.error(`Command "${command}" not found in the file.`);
+        }
+    });
+}
+
+
 module.exports = {
-    removeNonAlphanumeric
+    removeNonAlphanumeric,
+    logCommandEvent
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ const {getUser} = require('./user.js');
 const {getStats} = require('./stats.js');
 const {getPictures} = require('./pictures.js');
 const {getInfo} = require('./info.js');
+const {logCommandEvent} = require('./functions.js');
 const fs = require("fs");
 
 
@@ -65,49 +66,6 @@ function logServerEvent(eventType, guild) {
     });
 }
 
-function logCommandEvent(command){
-    const filePath = `./commands.txt`;
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error reading the file: ${err}`);
-            return;
-        }
-
-        // Create a map to store the values for each command
-        const valueMap = new Map();
-
-        // Split the file content by lines and parse each line
-        data.split('\n').forEach((line) => {
-            const [key, value] = line.trim().split(':');
-            if (key && value) {
-                valueMap.set(key.trim(), parseInt(value.trim()));
-            }
-        });
-
-        // Increment the value for the specified command
-        if (valueMap.has(command)) {
-            valueMap.set(command, valueMap.get(command) + 1);
-
-            // Generate the updated content
-            const updatedContent = Array.from(valueMap)
-                .map(([key, value]) => `${key}: ${value}`)
-                .join('\n');
-
-            // Write the updated content back to the file
-            fs.writeFile(filePath, updatedContent, 'utf8', (err) => {
-                if (err) {
-                    console.error(`Error writing to the file: ${err}`);
-                    return;
-                }
-                console.log(`${command} incremented successfully!`);
-            });
-        } else {
-            console.error(`Command "${command}" not found in the file.`);
-        }
-    });
-}
-
-
 // how the bot reacts to messages
 client.on('messageCreate', msg => {
 
@@ -166,4 +124,4 @@ client.on('messageCreate', msg => {
 })
 
 // login
-client.login(process.env.TOKEN).then(r => console.log(r)).catch((e) => console.log(e))
+client.login(process.env.DEV_TOKEN).then(r => console.log(r)).catch((e) => console.log(e))
