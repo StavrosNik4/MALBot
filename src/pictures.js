c283b3adebbdd591c4259c97ac37a5c9565ead5c
Stavros Nikolaidis <stavniko@csd.auth.gr>
2023-11-15 22:44:31 +0200

    optimized getPictures and getStats by using async

--- src/pictures.js
+++ src/pictures.js
@@ -1,5 +1,5 @@
 const malScraper = require("mal-scraper");
-const {removeNonAlphanumeric} = require("./functions.js");
+const { removeNonAlphanumeric } = require("./functions.js");
 
 /**
  * Function that returns pictures of an anime series.
@@ -7,100 +7,65 @@ const {removeNonAlphanumeric} = require("./functions.js");
  * @param query {string} Could be the MAL URL or the name of the anime.
  * @returns {Promise<string>} A Promise that resolves to a string with anime pictures or an error message.
  */
-function getPictures(query) {
-    return new Promise((resolve) => {
+async function getPictures(query) {
+    try {
         // Step 1: Search for anime based on the query
-        malScraper.getResultsFromSearch(query)
-            .then((data) => {
-                // Step 2: Filter the search results to match the query
-                const filteredData = data.filter((anime) => {
-                    const animeName = removeNonAlphanumeric(anime.name.toLowerCase());
-                    const queryStr = removeNonAlphanumeric(query.toLowerCase());
-                    return animeName.includes(queryStr);
-                });
-                // Step 3: Fetch statistics for each filtered anime
-                const promises = filteredData.map((anime) => {
-                    return malScraper.getStats({
-                        name: anime.name,
-                        id: anime.id
-                    }).then((stats) => {
-                        anime.total = stats.total;
-                        return anime;
-                    }).catch((err) => {
-                        console.log(`Failed to get stats for ${anime.name}: ${err}`);
-                        anime.total = -1;
-                        return anime;
-                    });
-                });
+        const data = await malScraper.getResultsFromSearch(query);
 
-                // Step 4: Wait for all promises to resolve
-                Promise.all(promises)
-                    .then((animeData) => {
-                        // Step 5: Find the most popular anime without sorting
-                        let mostPopularAnime = animeData[0];
+        // Step 2: Filter the search results to match the query
+        const filteredData = data.filter((anime) => {
+            const animeName = removeNonAlphanumeric(anime.name.toLowerCase());
+            const queryStr = removeNonAlphanumeric(query.toLowerCase());
+            return animeName.includes(queryStr);
+        });
 
-                        for (let i = 1; i < animeData.length; i++)
-                            if (animeData[i].total > mostPopularAnime.total)
-                                mostPopularAnime = animeData[i];
+        // Step 3: Fetch statistics for each filtered anime
+        const promises = filteredData.map(async (anime) => {
+            try {
+                const stats = await malScraper.getStats({ name: anime.name, id: anime.id });
+                anime.total = stats.total;
+                return anime;
+            } catch (err) {
+                console.log(`Failed to get stats for ${anime.name}: ${err}`);
+                anime.total = -1;
+                return anime;
+            }
+        });
 
-                        // Step 6: Fetch pictures for the most popular anime
-                        try{
-                            malScraper.getPictures({
-                                name: mostPopularAnime.name.toString(),
-                                id: parseInt(mostPopularAnime.id)
-                            })
-                                .then((data) => {
-                                    // Step 7: Build a result string with picture details
-                                    let resultString = "";
-                                    const finalObj = {}
-                                    for(let property1 in data){
-                                        Object.assign(finalObj, data[property1])
-                                        for(let property2 in finalObj) {
-                                            resultString = resultString + property2 + ': ' + finalObj[property2] + '\n'
-                                        }
-                                    }
-                                    if (resultString !== "")
-                                        resolve(resultString); // Resolve the Promise with the result string
-                                    else
-                                        resolve("This series doesn't exists!");
-                                })
-                                .catch((err) => {
-                                    resolve({resultString: "Error!", resultString2: "Try again!"});
-                                })
-                        }
-                        catch (err) {
-                            // Step 8: Handle exceptions when fetching pictures
-                            malScraper.getPictures(query)
-                                .then((data) => {
-                                    // Step 9: Build a result string with picture details
-                                    let resultString = "";
-                                    const finalObj = {}
-                                    for(let prop1 in data){
-                                        Object.assign(finalObj, data[prop1])
-                                        for(let prop2 in finalObj) {
-                                            resultString = resultString + prop2 + ': ' + finalObj[prop2] + '\n'
-                                        }
-                                    }
-                                    if (resultString !== "")
-                                        resolve(resultString); // Resolve the Promise with the result string
-                                    else
-                                        resolve("This series doesn't exists!");
-                                })
-                                .catch((err) => {
-                                    resolve("Error!\nTry again!");
-                                });
-                        }
-                    })
-                    .catch((err) => {
-                        resolve("Error!\nTry again!")
-                    });
-            })
-            .catch((err) => {
-                resolve("Error!\nTry again!")
-            });
-    });
+        // Step 4: Wait for all promises to resolve
+        const animeData = await Promise.all(promises);
+
+        // Step 5: Find the most popular anime without sorting
+        let mostPopularAnime = animeData.reduce((prev, current) => (current.total > prev.total) ? current : prev);
+
+        // Step 6: Fetch pictures for the most popular anime
+        const picturesData = await malScraper.getPictures({
+            name: mostPopularAnime.name.toString(),
+            id: parseInt(mostPopularAnime.id)
+        });
+
+        // Step 7: Build a result string with picture details
+        let resultString = "";
+        for (let property1 in picturesData) {
+            const finalObj = Object.assign({}, picturesData[property1]);
+            for (let property2 in finalObj) {
+                resultString = resultString + property2 + ': ' + finalObj[property2] + '\n';
+            }
+        }
+
+        // Step 8: Check if resultString is not empty and return the result
+        if (resultString !== "") {
+            return resultString;
+        } else {
+            return "This series doesn't exist!";
+        }
+    } catch (err) {
+        // Step 9: Handle exceptions when fetching pictures
+        console.error("Error!\nTry again!", err);
+        return "Error!\nTry again!";
+    }
 }
 
 module.exports = {
     getPictures
-}
+};
--- src/stats.js
+++ src/stats.js
@@ -1,175 +1,110 @@
-// function that returns stats of a show in a visual way
 const malScraper = require("mal-scraper");
 const QuickChart = require("quickchart-js");
-const {removeNonAlphanumeric} = require("./functions");
+const { removeNonAlphanumeric } = require("./functions");
 
-function getStats(query){
+/**
+ * Function that returns stats of a show in a visual way.
+ *
+ * @param query {string} Could be the MAL URL or the name of the anime.
+ * @returns {Promise<string>} A Promise that resolves to a string with charts representing user states and ratings or an error message.
+ */
+async function getStats(query) {
+    try {
+        // Step 1: Search for anime based on the query
+        const data = await malScraper.getResultsFromSearch(query);
 
-    return new Promise((resolve) => {
+        // Step 2: Filter the search results to only include anime whose name contains the query
+        const filteredData = data.filter((anime) => {
+            const animeName = removeNonAlphanumeric(anime.name.toLowerCase());
+            const queryStr = removeNonAlphanumeric(query.toLowerCase());
+            return animeName.includes(queryStr);
+        });
 
-        malScraper.getResultsFromSearch(query)
-            .then((data) => {
-                // Filter the search results to only include anime whose name contains the query
-                const filteredData = data.filter((anime) => {
-                    const animeName = removeNonAlphanumeric(anime.name.toLowerCase());
-                    const queryStr = removeNonAlphanumeric(query.toLowerCase());
-                    return animeName.includes(queryStr);
-                });
-
-                const promises = filteredData.map((anime) => {
-                    return malScraper.getStats({
-                        name: anime.name,
-                        id: anime.id
-                    }).then((stats) => {
-                        anime.total = stats.total;
-                        return anime;
-                    }).catch((err) => {
-                        console.log(`Failed to get stats for ${anime.name}: ${err}`);
-                        anime.total = -1;
-                        return anime;
-                    });
-                });
-
-                Promise.all(promises)
-                    .then((animeData) => {
-                        let mostPopularAnime = animeData[0];
-
-                        for (let i = 1; i < animeData.length; i++)
-                            if (animeData[i].total > mostPopularAnime.total)
-                                mostPopularAnime = animeData[i];
-                        try{
-                            malScraper.getStats({
-                                name: mostPopularAnime.name.toString(),
-                                id: parseInt(mostPopularAnime.id)
-                            }).then(async (data) => {
-                                let states = []
-                                for (let prop in data)
-                                    states.push(data[prop])
-                                const chart = new QuickChart();
-                                chart.setConfig({
-                                    type: 'bar',
-                                    data: {
-                                        labels: ['Watching', 'Completed', 'On Hold', 'Dropped', 'PTW'],
-                                        datasets: [{
-                                            label: 'User State',
-                                            data: [states[0], states[1], states[2], states[3], states[4]],
-                                            backgroundColor: '#fd5b75',
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }]
-                                    },
-                                    options: {
-                                        plugins: {
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }
-                                    }
-                                });
-                                let url = await chart.getShortUrl();
-                                let tmp = `User States: ${url}\n`;
-                                chart.setConfig({
-                                    type: 'bar',
-                                    data: { labels: ['1-2', '3-4', '5-6', '7-8', '9-10'],
-                                        datasets: [{
-                                            label: 'User Rating',
-                                            data: [states[15] + states[14], states[13] + states[12], states[11] +
-                                            states[10], states[9] + states[8], states[7] + states[6] ],
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        },
-                                        ]
-                                    },
-                                    options: {
-                                        plugins: {
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }
-                                    }
-                                });
-                                url = await chart.getShortUrl();
-                                resolve(tmp + `User Rating: ${url}`);
-                            }).catch((err) => {
-                                resolve({rpm: "Error!", rpm2: "Try again!"})
-                            });
-                        }
-                        catch (e){
-                            malScraper.getStats(query).then(async (data) => {
-                                let states = []
-                                for (let prop in data)
-                                    states.push(data[prop])
-                                const chart = new QuickChart();
-                                chart.setConfig({
-                                    type: 'bar',
-                                    data: {
-                                        labels: ['Watching', 'Completed', 'On Hold', 'Dropped', 'PTW'],
-                                        datasets: [{
-                                            label: 'User State',
-                                            data: [states[0], states[1], states[2], states[3], states[4]],
-                                            backgroundColor: '#fd5b75',
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }]
-                                    },
-                                    options: {
-                                        plugins: {
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }
-                                    }
-                                });
-                                let url = await chart.getShortUrl();
-                                let tmp = `User States: ${url}\n`;
-                                chart.setConfig({
-                                    type: 'bar',
-                                    data: { labels: ['1-2', '3-4', '5-6', '7-8', '9-10'],
-                                        datasets: [{
-                                            label: 'User Rating',
-                                            data: [states[15] + states[14], states[13] + states[12], states[11] +
-                                            states[10], states[9] + states[8], states[7] + states[6] ],
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }]
-                                    },
-                                    options: {
-                                        plugins: {
-                                            datalabels: {
-                                                color: '#000000',
-                                                yAdjust: 25
-                                            }
-                                        }
-                                    }
-                                });
-                                url = await chart.getShortUrl();
-                                resolve(tmp + `User Rating: ${url}`);
-                            }).catch((err) => {
-                                resolve("Error!\nTry again!");
-                            });
-                        }
-                    })
-                    .catch((err) => {
-                        resolve("Error!\nTry again!");
-                    });
-            })
-            .catch((err) => {
-                resolve("Error!\nTry again!")
+        // Step 3: Fetch statistics for each filtered anime
+        const promises = filteredData.map((anime) => {
+            return malScraper.getStats({
+                name: anime.name,
+                id: anime.id
+            }).then((stats) => {
+                anime.total = stats.total;
+                return anime;
+            }).catch((err) => {
+                console.log(`Failed to get stats for ${anime.name}: ${err}`);
+                anime.total = -1;
+                return anime;
             });
+        });
+
+        // Step 4: Wait for all promises to resolve
+        const animeData = await Promise.all(promises);
+
+        // Step 5: Find the most popular anime without sorting
+        let mostPopularAnime = animeData.reduce((prev, current) => (current.total > prev.total) ? current : prev);
+
+        // Step 6: Fetch statistics for the most popular anime
+        const dataStats = await malScraper.getStats({
+            name: mostPopularAnime.name.toString(),
+            id: parseInt(mostPopularAnime.id)
+        });
+
+        // Step 7: Extract data for user states and ratings
+        const states = [];
+        for (let prop in dataStats) {
+            states.push(dataStats[prop]);
+        }
+
+        // Step 8: Create a chart for user states
+        const chartUserStates = createChart(['Watching', 'Completed', 'On Hold', 'Dropped', 'PTW'], [states[0], states[1], states[2], states[3], states[4]]);
+        const urlUserStates = await chartUserStates.getShortUrl();
+
+        // Step 9: Create a chart for user ratings
+        const chartUserRating = createChart(['1-2', '3-4', '5-6', '7-8', '9-10'], [states[15] + states[14], states[13] + states[12], states[11] + states[10], states[9] + states[8], states[7] + states[6]]);
+        const urlUserRating = await chartUserRating.getShortUrl();
+
+        // Step 10: Build and return the result string with chart URLs
+        const resultString = `User States: ${urlUserStates}\nUser Rating: ${urlUserRating}`;
+        return resultString;
+    } catch (err) {
+        console.error("Error!\nTry again!", err);
+        return "Error!\nTry again!";
+    }
+}
+
+/**
+ * Helper function to create a QuickChart instance with specified data and options.
+ *
+ * @param labels {string[]} Labels for the chart.
+ * @param data {number[]} Data values for the chart.
+ * @returns {QuickChart} QuickChart instance.
+ */
+function createChart(labels, data) {
+    const chart = new QuickChart();
+    chart.setConfig({
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Data',
+                data: data,
+                backgroundColor: '#fd5b75',
+                datalabels: {
+                    color: '#000000',
+                    yAdjust: 25
+                }
+            }]
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    color: '#000000',
+                    yAdjust: 25
+                }
+            }
+        }
     });
+    return chart;
 }
 
 module.exports = {
     getStats
-}
+};
